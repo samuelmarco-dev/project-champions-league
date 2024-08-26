2fde081f950cc0bdc4a91df140f9f0dd3c1c86d0
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2024-08-26 00:53:45 -0300

    setup: CORS installation and request origin configuration

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,6 @@ import app from './app.js';
 const defaultPort = 4000;
 const port = Number(process.env.PORT) || defaultPort;
 
-app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
+app.listen(port, () =>
+    console.log(`Server running on port http://localhost:${port}`)
+);
