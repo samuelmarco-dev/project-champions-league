9a798d8c0783df3b4959160420f998bb0e6f2a89
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2024-08-26 00:52:51 -0300

    feat: separation of responsibilities using the layered architecture concept

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import express from 'express';
-
-const app = express();
+import app from './app.js';
 
 const defaultPort = 4000;
 const port = Number(process.env.PORT) || defaultPort;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
