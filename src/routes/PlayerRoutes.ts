be1835da0d8a695a72e3d9399a94c318dba6b1e6
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2024-08-27 12:37:10 -0300

    feat: update PlaterRoutes.ts

--- src/routes/PlayerRoutes.ts
+++ src/routes/PlayerRoutes.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import { playerController } from '../controllers/playerController.js';
 
 const PlayerRoutes = Router();
+
+PlayerRoutes.get('/', playerController.getAllPlayers);
+PlayerRoutes.get('/:id', playerController.getPlayerById);
+PlayerRoutes.post('/', playerController.postPlayer);
+PlayerRoutes.delete('/:id', playerController.deletePlayerById);
+PlayerRoutes.patch('/:id', playerController.patchPlayerById);
 
 export default PlayerRoutes;
